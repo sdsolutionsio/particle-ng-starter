3d964e4affa2e28224110669450e9b96170efa25
Nick DiMola <nick@sdsolutions.io>
2022-04-04 10:26:50 -0400

    Close menu in mobile when you click a link

--- src/app/layout/menu/menu.component.ts
+++ src/app/layout/menu/menu.component.ts
@@ -21,6 +21,10 @@ export class MenuComponent {
 
   navigate(page: string, fragment: string): void {
     this.router.navigate([page], {fragment}).then();
+
+    if (window.innerWidth <= 768) {
+      this.pushContainerLeft.close();
+    }
   }
 
 }
